a5f8c50cd251999b4b387d2ffc1ae6bf220d4057
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-01 09:38:26 -0600

    Adds and passes knows main characters

--- lib/network.rb
+++ lib/network.rb
@@ -8,6 +8,19 @@ class Network
   end
 
   def add_show(show)
-    @shows << show 
+    @shows << show
+  end
+
+  def main_characters
+    main_characters = []
+    @shows.each do |show|
+      show.characters.find_all do |character|
+        if (character.name == character.name.upcase) &&
+          character.salary >= 500_000
+          main_characters << character
+        end
+      end
+    end
+    main_characters
   end
 end
